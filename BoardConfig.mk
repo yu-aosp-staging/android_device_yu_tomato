c6f91bc3863ab9da8a7c42d2ae7409c60fbbebb7
Louis Popi <theh2o64@gmail.com>
2016-03-09 19:59:28 +0100

    tomato: Initial tree

---
+++ BoardConfig.mk
@@ -0,0 +1,90 @@
+#
+# Copyright (C) 2016 YU AOSP Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# inherit from common msm8916_64
+-include device/qcom/msm8916_64/BoardConfig.mk
+
+# Platform
+TARGET_NO_BOOTLOADER := true
+TARGET_CPU_CORTEX_A53 := true
+
+# Audio
+AUDIO_FEATURE_ENABLED_KPI_OPTIMIZE := true
+AUDIO_FEATURE_LOW_LATENCY_PRIMARY := true
+AUDIO_FEATURE_ENABLED_AUDIOSPHERE := true
+BOARD_USES_ALSA_AUDIO := true
+TARGET_USES_QCOM_MM_AUDIO := true
+USE_CUSTOM_AUDIO_POLICY := 1
+
+# Bluetooth
+BOARD_HAVE_BLUETOOTH := true
+BOARD_HAVE_BLUETOOTH_QCOM := true
+BLUETOOTH_HCI_USE_MCT := true
+
+# Block based OTA
+BLOCK_BASED_OTA := false
+
+# Camera
+TARGET_USES_AOSP := true
+
+# FM
+AUDIO_FEATURE_ENABLED_FM := true
+
+# Init
+TARGET_LIBINIT_DEFINES_FILE := device/yu/tomato/init/init_tomato.cpp
+
+# Kernel
+BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci lpm_levels.sleep_disabled=1
+KERNEL_DEFCONFIG := tomato_defconfig
+
+# Partitions
+BOARD_BOOTIMAGE_PARTITION_SIZE := 20971520
+BOARD_CACHEIMAGE_PARTITION_SIZE := 134217728
+BOARD_PERSISTIMAGE_PARTITION_SIZE := 10485760
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 20971520
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 1258291200
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 13576175616 # 13576192000 - 16384
+
+# Qualcomm support
+BOARD_USES_QCOM_HARDWARE := false
+
+# Recovery
+# TARGET_RECOVERY_FSTAB := device/yu/tomato/ramdisk/fstab.qcom TODO:Add me
+TARGET_RECOVERY_PIXEL_FORMAT := ABGR_8888
+TARGET_RECOVERY_DENSITY := xhdpi
+TARGET_USERIMAGES_USE_EXT4 := true
+TARGET_USERIMAGES_USE_F2FS := true
+
+# Releasetools
+TARGET_RELEASETOOLS_EXTENSIONS := device/yu/tomato
+
+# SELinux
+include device/qcom/sepolicy/sepolicy.mk
+
+# Wifi
+BOARD_HAS_QCOM_WLAN := true
+BOARD_HAS_QCOM_WLAN_SDK := true
+BOARD_HOSTAPD_DRIVER := NL80211
+BOARD_HOSTAPD_PRIVATE_LIB := lib_driver_cmd_qcwcn
+BOARD_WLAN_DEVICE := qcwcn
+BOARD_WPA_SUPPLICANT_DRIVER := NL80211
+BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_qcwcn
+WIFI_DRIVER_MODULE_PATH := "/system/lib/modules/wlan.ko"
+WIFI_DRIVER_MODULE_NAME := "wlan"
+WPA_SUPPLICANT_VERSION := VER_0_8_X
+
+# inherit from the proprietary version
+-include vendor/yu/tomato/BoardConfigVendor.mk
---
+++ device.mk
@@ -0,0 +1,66 @@
+#
+# Copyright (C) 2016 YU AOSP Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+$(call inherit-product-if-exists, vendor/yu/tomato/tomato-vendor.mk)
+
+$(call inherit-product, frameworks/native/build/phone-xhdpi-2048-dalvik-heap.mk)
+
+# CodeAurora msm8916_64 Tree
+include device/qcom/msm8916_64/msm8916_64.mk
+
+# Overlay
+# DEVICE_PACKAGE_OVERLAYS += device/yu/tomato/overlay
+# PRODUCT_PACKAGE_OVERLAYS += device/yu/tomato/overlay
+
+# Screen density
+PRODUCT_AAPT_CONFIG := normal
+PRODUCT_AAPT_PREF_CONFIG := xxhdpi
+
+# Charger
+ADDITIONAL_DEFAULT_PROPERTIES += \
+    ro.usb.id.charge=F006 \
+    ro.usb.id.mtp=F003 \
+    ro.usb.id.mtp_adb=9039 \
+    ro.usb.id.ptp=904D \
+    ro.usb.id.ptp_adb=904E \
+    ro.usb.id.ums=F000 \
+    ro.usb.id.ums_adb=9015 \
+    ro.usb.vid=05c6
+
+# GPS
+PRODUCT_PACKAGES += \
+    gps.msm8916
+
+# Keystore
+PRODUCT_PACKAGES += \
+    keystore.qcom
+
+# Model
+PRODUCT_PACKAGES += \
+    ro.product.model=YUREKA
+
+# Power HAL
+PRODUCT_PACKAGES += \
+    power.qcom
+
+# Sensors
+PRODUCT_PACKAGES += \
+    sensors.msm8916
+
+# Wifi
+PRODUCT_PACKAGES += \
+    libwpa_client \
+    wcnss_service
